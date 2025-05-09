1c175fa54dcb5473f1c2a7faeb5ca9fb2434a0b7
leonardo <leonardo.xavgarcia@gmail.com>
2025-05-09 13:28:39 -0300

    primeiro commit

---
+++ filmes/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from filmes.forms import FilmeForm
+from sistema.models import Filme
+
+
+def cadastrarFilme(request):
+
+    if request.method == 'POST':
+        form = FilmeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listarfilmes')
+    else:
+        form = FilmeForm()
+
+    return render(
+        request, 
+        'filmes/cadastrar.html', 
+        {'form': form},
+        )
+
+
+def listarFilmes(request):
+    filmes = Filme.objects.all()
+    
+    context = {
+        'filmes': filmes,
+    }
+    
+    return render(
+        request,
+        'filmes/listar.html',
+        context,
+    )
